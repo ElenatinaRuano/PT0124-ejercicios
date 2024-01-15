8e121ac6e78d48f49e709e1856a9962aef329c1d
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-01-15 19:48:07 +0100

    Iteracion#2

--- Exercises/neoland-basic-algorithms.js
+++ Exercises/neoland-basic-algorithms.js
@@ -1,10 +1,37 @@
-//Iteración#1
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#1
 
-let myFavoriteHero = "Hulk";
+/* let myFavoriteHero = "Hulk";
 let x = 50;
 let h = 5;
 let y = 10;
-let z = h + y;
+let z = h+y;
 
-console.log(z);
+console.log(); */
+
+
+
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#𝟮
+
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+console.log(character.age);
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`);
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log(toy1.price + toy2.price);
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+globalBasePrice = 25000;
+car1.finalPrice = car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log(car1.finalPrice);
+console.log(car2.finalPrice);
+
 
