34f9f10c83dfcf326d64c047b1f4e3e38688f587
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-01-15 20:25:22 +0100

    Iteracion#5

--- Exercises/neoland-basic-algorithms.js
+++ Exercises/neoland-basic-algorithms.js
@@ -1,4 +1,4 @@
-//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#1
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#1: Variables
 
 /* let myFavoriteHero = "Hulk";
 let x = 50;
@@ -10,7 +10,7 @@ console.log(); */
 
 
 
-//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#𝟮
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#𝟮: Variables avanzadas
 
 /* const character = {name: 'Jack Sparrow', age: 10};
 character.age = 25;
@@ -36,7 +36,7 @@ console.log(car2.finalPrice); */
 
 
 
-//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#3
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#3: Operadores
 /* console.log(10*5);
 console.log(10/2);
 console.log(15%9);
@@ -51,8 +51,8 @@ let i = 10*5; */
 
 
 
-//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#4 (en GIT me he equivocado y he subido dos veces la Iteracion3)
-const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#4: Arrays (en GIT me he equivocado y he subido dos veces la Iteracion3)
+/* const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 console.log(avengers[0]);
 
 avengers[0]="IRONMAN";
@@ -74,5 +74,39 @@ console.log(`El últumo elemento de la lista es ${rickAndMortyCharacters[rickAnd
 
 rickAndMortyCharacters.push("Lapiz Lopez")
 rickAndMortyCharacters.splice(1,1);
-console.log(rickAndMortyCharacters);
+console.log(rickAndMortyCharacters); */
 
+
+
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#5: Condicionales
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (number2/number1 == 2) {
+    console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+    console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+    console.log("number3 es distinto number1");
+}
+
+if (number3*5 == number1) {
+    console.log("number3 por 5 es igual a number1");
+}
+
+if (number3*5 == number1 && number1*2 == number2) {
+    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2/2 == number1 || number1/5 == number3) {
+    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
