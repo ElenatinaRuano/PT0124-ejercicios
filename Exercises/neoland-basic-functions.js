7cac1f0667940419bbd576bf7085bc045f7ac7d7
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-01-22 19:07:46 +0100

    JS Functions_Iteration#1: Buscar el maximo

---
+++ Exercises/neoland-basic-functions.js
@@ -0,0 +1,12 @@
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#1: Buscar el máximo
+function sum(numberOne , numberTwo) {
+    if (numberOne>numberTwo){
+        return numberOne
+    }
+
+    else {
+        return numberTwo
+    }
+}
+
+console.log(sum(1,5))
