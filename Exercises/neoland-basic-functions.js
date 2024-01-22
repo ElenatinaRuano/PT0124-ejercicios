e3abc8c364a5842778db6ac67cdbad3e0e0ebe30
ElenatinaRuano <ruano.herrera.elena.tic@gmail.com>
2024-01-22 19:09:34 +0100

    JS Functions_Iteration#2: Buscar la palabra mas larga

--- Exercises/neoland-basic-functions.js
+++ Exercises/neoland-basic-functions.js
@@ -1,5 +1,5 @@
 //* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#1: Buscar el máximo
-function sum(numberOne , numberTwo) {
+/* function sum(numberOne , numberTwo) {
     if (numberOne>numberTwo){
         return numberOne
     }
@@ -9,4 +9,27 @@ function sum(numberOne , numberTwo) {
     }
 }
 
-console.log(sum(1,5))
+console.log(sum(1,5)) */
+
+
+
+//* 𝗜𝘁𝗲𝗿𝗮𝗰𝗶ó𝗻#2: Buscar la palabra más larga
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+    
+    let longMax = 0;
+    let finalWord;
+
+    for(let item of param){
+
+        if (item.length > longMax) {
+            longMax = item.length;
+            finalWord = item;
+        }
+    }
+
+    return finalWord;
+}
+
+console.log(findLongestWord(avengers));
